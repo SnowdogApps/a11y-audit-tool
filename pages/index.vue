3a1fd1c9e105e5aaf668da2d59d4bcf1fe9b2ad7
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-05-02 11:46:09 +0200

    #99543 - add temporary Dashboard

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Dashboard</div>
+  <NuxtLink to="login"> Log In </NuxtLink><br />
+  <NuxtLink to="register"> Register </NuxtLink>
+</template>
