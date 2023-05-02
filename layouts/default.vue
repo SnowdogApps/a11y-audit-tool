728b7ab88f12a36059cf9016dae9919b025a9df3
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-05-02 11:58:19 +0200

    #99543 - add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    A *default* layout
+    <slot />
+  </div>
+</template>
---
+++ layouts/simple.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center px-5">
+    <div
+      class="surface-border surface-card border-round z-1 border px-4 py-7 md:px-7"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
