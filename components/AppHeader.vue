373edcc68abfb9d2405ca3fdc02fed6795403481
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-05-23 23:26:38 +0200

    Simplify profile menu code

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import IconLogo from 'assets/icons/logo.svg'
+import { ProfileMenuLink } from '~/types/profile-menu-link'
 
 defineProps<{
   isSideNavigationVisible: boolean
@@ -7,8 +7,35 @@ defineProps<{
 
 defineEmits<{ (e: 'toggle-main-menu'): void }>()
 
+const profileMenuList = [
+  {
+    iconClasses: 'pi pi-user text-xl text-primary',
+    url: '/account',
+    text: 'Profile',
+    subtitle: 'Account details',
+  },
+  {
+    iconClasses: 'pi pi-money-bill text-xl text-primary',
+    url: '/account/billing',
+    text: 'Billing',
+    subtitle: 'Check your fees',
+  },
+  {
+    iconClasses: 'pi pi-cog text-xl text-primary',
+    url: '/account/settings',
+    text: 'Settings',
+    subtitle: 'Select preferences',
+  },
+  {
+    iconClasses: 'pi pi-power-off text-xl text-primary',
+    url: '/auth/sign-out',
+    text: 'Sign out',
+  },
+] satisfies Partial<ProfileMenuLink>[]
+
 const isProfileMenuVisible = ref(false)
 </script>
+
 <template>
   <div class="my-4 flex w-full items-center justify-between">
     <Button
@@ -22,10 +49,14 @@ const isProfileMenuVisible = ref(false)
     />
 
     <Button
-      class="p-button-link border !border-gray-500 rounded !rounded-full !p-0 !m-0"
+      class="p-button-link !m-0 !rounded-full rounded border !border-gray-500 !p-0"
       @click="isProfileMenuVisible = true"
     >
-      <img alt="" src="/avatar.svg" class="w-10 h-10" />
+      <img
+        alt=""
+        src="/avatar.svg"
+        class="h-10 w-10"
+      />
     </Button>
 
     <Sidebar
@@ -35,53 +66,17 @@ const isProfileMenuVisible = ref(false)
       <div class="p-2">
         <strong>Welcome</strong>
         <p>Anonymous</p>
-        <ul class="grid gap-4 mt-4">
-          <li>
-            <NuxtLink
-              class="w-full flex p-3 items-center border-neutral-400
-              border rounded border-solid hover:bg-neutral-100
-               transition-colors transition-duration-150"
-              to="/account"
-            >
-              <span><i class="pi pi-user text-xl text-primary"></i></span>
-              <div class="ml-3">
-                <span class="mb-2 font-semibold">Profile</span>
-                <p class="text-color-secondary m-0">Account details</p>
-              </div>
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              class="w-full flex p-3 items-center border-neutral-400
-              border rounded border-solid hover:bg-neutral-100
-               transition-colors transition-duration-150"
-              to="/account/billing"
-            >
-              <span><i class="pi pi-money-bill text-xl text-primary"></i></span>
-              <div class="ml-3"><span class="mb-2 font-semibold">Billing</span><p class="text-color-secondary m-0">Check your fees</p></div>
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              class="w-full flex p-3 items-center border-neutral-400
-              border rounded border-solid hover:bg-neutral-100
-               transition-colors transition-duration-150"
-              to="/account/settings"
-            >
-              <span><i class="pi pi-cog text-xl text-primary"></i></span>
-              <div class="ml-3"><span class="mb-2 font-semibold">Settings</span><p class="text-color-secondary m-0">Exercitation veniam</p></div>
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              class="w-full flex p-3 items-center border-neutral-400
-              border rounded border-solid hover:bg-neutral-100
-               transition-colors transition-duration-150"
-              to="/auth/sign-out"
-            >
-              <span><i class="pi pi-power-off text-xl text-primary"></i></span>
-              <div class="ml-3"><span class="mb-2 font-semibold">Sign Out</span></div>
-            </NuxtLink>
+        <ul class="mt-4 grid gap-4">
+          <li
+            v-for="{ iconClasses, url, text, subtitle } in profileMenuList"
+            :key="`profile-menu-link-${text}`"
+          >
+            <ProfileMenuLink
+              :icon-classes="iconClasses"
+              :url="url"
+              :text="text"
+              :subtitle="subtitle || ''"
+            />
           </li>
         </ul>
       </div>
---
+++ types/profile-menu-link.ts
@@ -0,0 +1,6 @@
+export interface ProfileMenuLink {
+  iconClasses: string
+  url: string
+  text: string
+  subtitle: string
+}
