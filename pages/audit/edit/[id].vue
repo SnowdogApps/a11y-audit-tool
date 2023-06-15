f5cf745b2a159e273ba15b61ef194ad7c0be4c7a
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-06-15 13:04:51 +0200

    Style audit list and add edit page (in preparation)

---
+++ pages/audit/edit/[id].vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'auth',
+})
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="grid">
+    <h1>Edit audit #{{ route.params.id }}</h1>
+  </div>
+
+  <Card>
+    <template #content> page in preparation </template>
+  </Card>
+</template>
