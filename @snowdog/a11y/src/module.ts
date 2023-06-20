6feb0680d298b364e9f63ff640131daf175d7466
Robert Woźniak <robert.wozniak@snow.dog>
2023-06-20 15:07:43 +0200

    #99580 - Add api test basic check

--- @snowdog/a11y/src/module.ts
+++ @snowdog/a11y/src/module.ts
@@ -1,5 +1,9 @@
 import { fileURLToPath } from 'url'
-import { defineNuxtModule, addServerHandler, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addServerHandler,
+  createResolver
+} from '@nuxt/kit'
 
 export interface ModuleOptions {
   routeName: string
@@ -25,6 +29,8 @@ export default defineNuxtModule<ModuleOptions>({
     const metaUrl = import.meta.url
     const runtimeDir = fileURLToPath(new URL('./runtime', metaUrl))
     const { resolve } = createResolver(metaUrl)
+
+    nuxt.options.build.transpile.push(runtimeDir)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addServerHandler({
--- @snowdog/a11y/src/runtime/server/api/test.ts
+++ @snowdog/a11y/src/runtime/server/api/test.ts
@@ -1,6 +1,18 @@
-import { assertMethod, defineEventHandler } from 'h3'
+import { assertMethod, defineEventHandler, createError, getQuery } from 'h3'
 
 export default defineEventHandler(async (event) => {
   assertMethod(event, 'GET')
-  return 'test init goes here'
+
+  const { baseUrl } = getQuery(event)
+
+  if (!baseUrl) {
+    throw createError({
+      message: 'baseUrl var is mandatory',
+      statusCode: 422,
+    })
+  }
+
+  // @todo: action
+
+  return [{ foo: 'bar' }]
 })
