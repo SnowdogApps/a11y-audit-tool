4121e3c25d74ca36550f82b0d30f85dda57c95cb
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-06-23 17:31:32 +0200

    Fix alias path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   alias: {
     types: resolve(__dirname, './types'),
     utils: resolve(__dirname, './utils'),
-    validation: resolve(__dirname, './utils'),
+    validation: resolve(__dirname, './validation'),
   },
   modules: [
     '@nuxtjs/tailwindcss',
