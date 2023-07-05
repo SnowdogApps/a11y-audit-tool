51daadff88b8f00dd757880e02a103d22c7510d8
Robert Woźniak <robert.wozniak@snow.dog>
2023-07-05 13:15:22 +0200

    #101258 - Regenerate supabase types

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,12 +1,9 @@
-import type { User } from '@supabase/gotrue-js'
-import type { AuditConfiguration } from 'types/audit'
-
 export type Json =
   | string
   | number
   | boolean
   | null
-  | { [key: string]: Json }
+  | { [key: string]: Json | undefined }
   | Json[]
 
 export interface Database {
@@ -14,9 +11,10 @@ export interface Database {
     Tables: {
       audits: {
         Row: {
+          axe_results: Json | null
+          config: Json
           created_at: string
           id: number
-          config: AuditConfiguration | null
           issues: Json | null
           profile_id: string
           project_id: number
@@ -24,9 +22,10 @@ export interface Database {
           updated_at: string | null
         }
         Insert: {
+          axe_results?: Json | null
+          config?: Json
           created_at?: string
           id?: number
-          config?: AuditConfiguration
           issues?: Json | null
           profile_id: string
           project_id?: number
@@ -34,15 +33,30 @@ export interface Database {
           updated_at?: string | null
         }
         Update: {
+          axe_results?: Json | null
+          config?: Json
           created_at?: string
           id?: number
-          config?: AuditConfiguration
           issues?: Json | null
           profile_id?: string
           project_id?: number
           status?: string | null
           updated_at?: string | null
         }
+        Relationships: [
+          {
+            foreignKeyName: 'audits_profile_id_fkey'
+            columns: ['profile_id']
+            referencedRelation: 'profiles'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'audits_project_id_fkey'
+            columns: ['project_id']
+            referencedRelation: 'projects'
+            referencedColumns: ['id']
+          }
+        ]
       }
       profile_project: {
         Row: {
@@ -51,12 +65,26 @@ export interface Database {
         }
         Insert: {
           profile_id: string
-          project_id?: number
+          project_id: number
         }
         Update: {
           profile_id?: string
           project_id?: number
         }
+        Relationships: [
+          {
+            foreignKeyName: 'profile_project_profile_id_fkey'
+            columns: ['profile_id']
+            referencedRelation: 'profiles'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'profile_project_project_id_fkey'
+            columns: ['project_id']
+            referencedRelation: 'projects'
+            referencedColumns: ['id']
+          }
+        ]
       }
       profiles: {
         Row: {
@@ -83,6 +111,14 @@ export interface Database {
           user_type?: string | null
           username?: string | null
         }
+        Relationships: [
+          {
+            foreignKeyName: 'profiles_id_fkey'
+            columns: ['id']
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          }
+        ]
       }
       projects: {
         Row: {
@@ -106,10 +142,36 @@ export interface Database {
           name?: string
           updated_at?: string | null
         }
+        Relationships: []
       }
     }
     Views: {
-      [_ in never]: never
+      extended_audits: {
+        Row: {
+          created_at: string | null
+          id: number | null
+          issues: Json | null
+          profile_id: string | null
+          project: string | null
+          project_id: number | null
+          status: string | null
+          updated_at: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'audits_profile_id_fkey'
+            columns: ['profile_id']
+            referencedRelation: 'profiles'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'audits_project_id_fkey'
+            columns: ['project_id']
+            referencedRelation: 'projects'
+            referencedColumns: ['id']
+          }
+        ]
+      }
     }
     Functions: {
       delete_claim: {
@@ -162,9 +224,4 @@ export interface Database {
       [_ in never]: never
     }
   }
-}
-
-type Profile = Database['public']['Tables']['profiles']['Row']
-export interface ProfileWithEmail extends Profile {
-  email: string
 }
