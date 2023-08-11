afd68f9f7345bce2f0383fc0e21bd7683f36b1cc
Patryk Bura <patryk.bura@snow.dog>
2023-08-11 14:22:30 +0200

    #101762 - Forbid access for non auditors

--- pages/audit/new.vue
+++ pages/audit/new.vue
@@ -1,6 +1,28 @@
 <script lang="ts" setup>
+import type { Database } from 'types/supabase'
+import type { UserClaim } from 'types/user'
+
 definePageMeta({
-  middleware: 'auth',
+  middleware: [
+    'auth',
+    defineNuxtRouteMiddleware(async () => {
+      const supabase = useSupabaseClient<Database>()
+
+      const { data: claims } = (await supabase.rpc(
+        'get_my_claims'
+      )) as unknown as {
+        data: UserClaim
+      }
+
+      const isAuditor = claims?.user_role === 'auditor'
+
+      if (!isAuditor) {
+        abortNavigation({
+          message: 'You are not allowed to be here. Go away!',
+        })
+      }
+    }),
+  ],
 })
 </script>
 
