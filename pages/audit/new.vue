f5b6bf2e3465d9627955a089cda4fc537d477de3
Patryk Bura <patryk.bura@snow.dog>
2023-08-22 12:27:02 +0200

    #101762 - Use composable

--- pages/audit/new.vue
+++ pages/audit/new.vue
@@ -1,22 +1,11 @@
 <script lang="ts" setup>
-import type { Database } from 'types/supabase'
-import type { UserClaim } from 'types/user'
-
 definePageMeta({
   middleware: [
     'auth',
-    defineNuxtRouteMiddleware(async () => {
-      const supabase = useSupabaseClient<Database>()
+    defineNuxtRouteMiddleware(() => {
+      const { isAdmin, isAuditor } = useUser()
 
-      const { data: claims } = (await supabase.rpc(
-        'get_my_claims'
-      )) as unknown as {
-        data: UserClaim
-      }
-
-      const isAuditor = claims?.user_role === 'auditor'
-
-      if (!isAuditor) {
+      if (!isAuditor.value && !isAdmin.value) {
         abortNavigation({
           message: 'You are not allowed to be here. Go away!',
         })
