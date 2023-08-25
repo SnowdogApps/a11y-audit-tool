9412794dd70ecf5c4b03dd17c0b03c48a3dfcd6d
Patryk Bura <patryk.bura@snow.dog>
2023-08-25 15:04:01 +0200

    #99597 - Hide redundant data

--- components/TrustedTestInfo.vue
+++ components/TrustedTestInfo.vue
@@ -10,17 +10,23 @@ defineProps<{
       v-for="(tTValue, tTKey) in info"
       :key="tTKey"
     >
-      <span class="mr-2 inline-block font-bold first-letter:uppercase">
-        {{ tTKey }}:
-      </span>
-      <Tag
-        v-if="['Level', 'Test ID'].includes(tTKey)"
-        :value="tTValue || 'n/a'"
-        severity="info"
-        rounded
-        class="text-xs tracking-wider"
-      />
-      <span v-else>{{ tTValue || 'n/a' }}</span>
+      <div
+        v-if="
+          !['status', 'manualTestDesc', 'recommendationDesc'].includes(tTKey)
+        "
+      >
+        <span class="mr-2 inline-block font-bold first-letter:uppercase">
+          {{ tTKey }}:
+        </span>
+        <Tag
+          v-if="['Level', 'Test ID'].includes(tTKey)"
+          :value="tTValue || 'n/a'"
+          severity="info"
+          rounded
+          class="text-xs tracking-wider"
+        />
+        <span v-else>{{ tTValue || 'n/a' }}</span>
+      </div>
     </div>
   </div>
 </template>
