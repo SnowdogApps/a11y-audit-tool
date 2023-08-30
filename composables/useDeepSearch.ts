1627678148f6dc0c0c0c229f9edfef7fbee000a5
Patryk <patryk.bura@snow.dog>
2023-08-30 09:06:33 +0200

    #99597 - Use stack based approach

--- composables/useDeepSearch.ts
+++ composables/useDeepSearch.ts
@@ -1,24 +1,37 @@
 export function useDeepSearch(
   obj: Record<string, any>,
   target: string,
-  depth = 0,
-  maxDepth = 3,
-  foundObjects: Record<string, any>[] = []
+  maxDepth = 3
 ): Record<string, any>[] {
-  if (depth > maxDepth) {
-    return foundObjects
-  }
+  const foundObjects: Record<string, any>[] = []
+  const stack: { obj: Record<string, any>; depth: number }[] = [
+    { obj, depth: 0 },
+  ]
 
-  if (typeof obj === 'object' && obj !== null) {
-    for (const value of Object.values(obj)) {
-      if (
-        typeof value === 'string' &&
-        value.toLowerCase().includes(target.toLowerCase())
-      ) {
-        foundObjects.push(obj)
-        break
-      } else {
-        useDeepSearch(value, target, depth + 1, maxDepth, foundObjects)
+  while (stack.length > 0) {
+    const stackItem = stack.pop()
+
+    if (!stackItem) {
+      break
+    }
+
+    const { obj: currentObj, depth } = stackItem
+
+    if (depth > maxDepth) {
+      continue
+    }
+
+    if (typeof currentObj === 'object' && currentObj !== null) {
+      for (const value of Object.values(currentObj)) {
+        if (
+          typeof value === 'string' &&
+          value.toLowerCase().includes(target.toLowerCase())
+        ) {
+          foundObjects.push(currentObj)
+          break
+        } else if (typeof value === 'object' && value !== null) {
+          stack.push({ obj: value, depth: depth + 1 })
+        }
       }
     }
   }
