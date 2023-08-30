65d5cc6d3c0579120a86bbeec14cb5ae59092458
Patryk <patryk.bura@snow.dog>
2023-08-30 15:12:04 +0200

    #99597 - Resolving issues with changes in 102162

--- components/AuditAxeResult.vue
+++ components/AuditAxeResult.vue
@@ -10,6 +10,9 @@ const props = defineProps<{
 const { audit, isSaving, formData, updateField, saveFormData } = await useAudit(
   props.result
 )
+
+const { filteredAudit, optionLists, searchValue, selectedItems } =
+  useAuditFilters(audit, formData)
 </script>
 
 <template>
@@ -39,10 +42,76 @@ const { audit, isSaving, formData, updateField, saveFormData } = await useAudit(
       <ClientOnly>
         <TabView>
           <TabPanel
-            v-for="(type, typeIndex) in audit"
+            v-for="(type, typeIndex) in filteredAudit"
             :key="`type-${typeIndex}`"
             :header="type.name"
           >
+            <div v-if="typeIndex === 'wcagCoveredByTrustedTest'">
+              <div class="flex flex-row flex-wrap gap-4 p-4">
+                <div class="flex flex-row flex-wrap gap-2">
+                  <span class="flex flex-col">
+                    <label
+                      for="wcagScList"
+                      class="mb-2 text-xs"
+                    >
+                      WCAG SC
+                    </label>
+                    <MultiSelect
+                      id="wcagScList"
+                      v-model="selectedItems.selectedWcagSc"
+                      :options="optionLists.wcagScList"
+                      filter
+                      option-label="name"
+                      placeholder="WCAG SC"
+                      class="md:w-20rem"
+                      :max-selected-labels="3"
+                    />
+                  </span>
+                  <span class="flex flex-col">
+                    <label
+                      for="levelList"
+                      class="mb-2 text-xs"
+                    >
+                      Level
+                    </label>
+                    <MultiSelect
+                      id="levelList"
+                      v-model="selectedItems.selectedLevel"
+                      :options="optionLists.levelList"
+                      option-label="name"
+                      placeholder="Level"
+                      class="md:w-20rem"
+                      :max-selected-labels="2"
+                    />
+                  </span>
+                  <span class="flex flex-col">
+                    <label
+                      for="statusList"
+                      class="mb-2 text-xs"
+                    >
+                      Status
+                    </label>
+                    <MultiSelect
+                      id="statusList"
+                      v-model="selectedItems.selectedStatus"
+                      :options="optionLists.statusList"
+                      option-label="name"
+                      placeholder="Status"
+                      class="md:w-20rem"
+                      :max-selected-labels="2"
+                    />
+                  </span>
+                </div>
+                <div class="p-input-icon-left mt-auto h-10 grow">
+                  <i class="pi pi-search" />
+                  <InputText
+                    v-model="searchValue"
+                    placeholder="Search (3 characters minimum)"
+                    class="w-full max-w-3xl"
+                  />
+                </div>
+              </div>
+            </div>
             <DynamicScroller
               :items="type.tests"
               :min-item-size="317"
