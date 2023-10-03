b7b1fe8409b608af44a56ecfa70f1a52f62ca148
Ola <ola.frankowska@snow.dog>
2023-10-03 11:17:04 +0200

    #102563 - add skip navigation & go back to top buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,6 +18,8 @@ const menuItems = computed<MenuItem[]>(() =>
     ? auditorMenuItems
     : customerMenuItems
 )
+
+const { y: windowScrollY } = useWindowScroll()
 </script>
 
 <template>
@@ -28,15 +30,45 @@ const menuItems = computed<MenuItem[]>(() =>
         'layout-wrapper--slim': isSideNavigationVisible,
       }"
     >
+      <a
+        id="skip-navigation-link"
+        href="#main-content"
+        class="sr-only -translate-y-16 scale-90 transition focus:not-sr-only focus:absolute focus:z-20 focus:translate-y-0 focus:scale-100"
+      >
+        <div class="p-button">Skip Navigation</div>
+      </a>
       <AppHeader
         :is-side-navigation-visible="isSideNavigationVisible"
         @toggle-main-menu="isSideNavigationVisible = !isSideNavigationVisible"
       />
-      <main>
+      <main
+        id="main-content"
+        ref="mainContent"
+        tabindex="-1"
+      >
         <slot />
       </main>
+      <div
+        v-show="windowScrollY > 800"
+        class="fixed right-4 z-20 w-full"
+        :class="
+          typeof $route.name === 'string' && ['audit-id'].includes($route.name)
+            ? 'bottom-[80px]'
+            : 'bottom-4'
+        "
+      >
+        <div class="flex w-full justify-end px-4 pb-1 xl:container xl:mx-auto">
+          <a
+            class="p-button p-button-rounded p-button-icon-only !h-12 !w-12 !opacity-80 hover:!opacity-100"
+            rounded
+            aria-label="Go back to top"
+            href="#main-content"
+          >
+            <i class="pi pi-arrow-up" />
+          </a>
+        </div>
+      </div>
     </div>
-
     <AppSidebar
       :is-visible="isSideNavigationVisible"
       @close-main-menu="isSideNavigationVisible = false"
@@ -52,5 +84,11 @@ const menuItems = computed<MenuItem[]>(() =>
 <style>
 body {
   background-color: var(--surface-ground);
+}
+
+html {
+  @media (prefers-reduced-motion: no-preference) {
+    scroll-behavior: smooth;
+  }
 }
 </style>
