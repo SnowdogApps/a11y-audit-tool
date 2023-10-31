bc2636731954c6ea8ceba5bb2be663df56715418
Ola <ola.frankowska@snow.dog>
2023-10-31 10:58:06 +0100

    #102605 - add aria-live

--- components/AuditProcessingDialog.vue
+++ components/AuditProcessingDialog.vue
@@ -53,57 +53,59 @@ defineEmits<{
     <template #header>
       <h2 class="font-medium">New audit processing</h2>
     </template>
-    <div
-      v-if="isAuditFinished"
-      class="flex min-h-[200px] items-center justify-center"
-    >
-      <div v-if="axeErrorMessage">
-        <p class="mb-3">
-          <span class="mb-3 block text-center text-lg text-red-700">
-            Oops, something went wrong:
-          </span>
-          <span class="block whitespace-pre-line rounded-md bg-gray-100 p-4">
-            {{ axeErrorMessage }}
-          </span>
+    <div aria-live="polite">
+      <div
+        v-if="isAuditFinished"
+        class="flex min-h-[200px] items-center justify-center"
+      >
+        <div v-if="axeErrorMessage">
+          <p class="mb-3">
+            <span class="mb-3 block text-center text-lg text-red-700">
+              Oops, something went wrong:
+            </span>
+            <span class="block whitespace-pre-line rounded-md bg-gray-100 p-4">
+              {{ axeErrorMessage }}
+            </span>
+          </p>
+          <Button
+            class="!block !w-full"
+            outlined
+            @click="$emit('close', { resetAuditForm: false })"
+          >
+            Repeat audit
+          </Button>
+        </div>
+        <div v-else>
+          <p class="mb-6 text-center text-lg">
+            Automatic tests finished successfully!
+          </p>
+          <NuxtLink
+            class="p-button !block"
+            :to="`/audit/${auditId}`"
+          >
+            View audit results
+          </NuxtLink>
+        </div>
+      </div>
+      <template v-else>
+        <p class="mb-6 text-center text-lg">
+          Your audit has been initiated and is currently being processed. Please
+          wait a few minutes for the results to be generated. The audit results
+          will be provided in a link below and in the
+          <NuxtLink to="/audit">Audit List</NuxtLink>.
         </p>
+        <ProgressBar
+          class="mb-3"
+          mode="indeterminate"
+        />
         <Button
           class="!block !w-full"
-          outlined
-          @click="$emit('close', { resetAuditForm: false })"
+          text
+          @click="$emit('close')"
         >
-          Repeat audit
+          Create another audit
         </Button>
-      </div>
-      <div v-else>
-        <p class="mb-6 text-center text-lg">
-          Automatic tests finished successfully!
-        </p>
-        <NuxtLink
-          class="p-button !block"
-          :to="`/audit/${auditId}`"
-        >
-          View audit results
-        </NuxtLink>
-      </div>
+      </template>
     </div>
-    <template v-else>
-      <p class="mb-6 text-center text-lg">
-        Your audit has been initiated and is currently being processed. Please
-        wait a few minutes for the results to be generated. The audit results
-        will be provided in a link below and in the
-        <NuxtLink to="/audit">Audit List</NuxtLink>.
-      </p>
-      <ProgressBar
-        class="mb-3"
-        mode="indeterminate"
-      />
-      <Button
-        class="!block !w-full"
-        text
-        @click="$emit('close')"
-      >
-        Create another audit
-      </Button>
-    </template>
   </Dialog>
 </template>
