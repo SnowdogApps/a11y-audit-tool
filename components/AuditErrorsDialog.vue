8a1b30a265859a23f513338dc8815e8ae80a492a
Karol Osuchowski <karol.osuchowski@snow.dog>
2023-11-07 10:33:08 +0100

    #102415 - add dialog to display audit errors

---
+++ components/AuditErrorsDialog.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+defineProps<{
+  header: string
+  errorMessage: string
+}>()
+</script>
+
+<template>
+  <Dialog
+    modal
+    :header="header"
+    class="w-full max-w-xl"
+    dismissable-mask
+  >
+    <p class="mb-3">
+      <span class="mb-3 block text-center text-lg text-red-700">
+        Oops, something went wrong:
+      </span>
+      <span class="block whitespace-pre-line rounded-md bg-gray-100 p-4">
+        {{ errorMessage }}
+      </span>
+    </p>
+  </Dialog>
+</template>
