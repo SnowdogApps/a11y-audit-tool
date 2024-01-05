04ecdd7b21a45e1889f020ec349b81370d6dca25
Ola <ola.frankowska@snow.dog>
2024-01-05 15:55:17 +0100

    Revert "#104288 - fix link for audit without axe"
    This reverts commit 245a7c9bddff36bb0974df332b0ebba4be9e8ddd.

--- utils/get-audit-link.ts
+++ utils/get-audit-link.ts
@@ -7,21 +7,12 @@ export const getAuditLink = ({
   reportType,
 }: {
   id: Audit['id']
-  axeId?: Axe['id']
+  axeId: Axe['id']
   status: Audit['status']
   reportType: Audit['report_type']
 }) => {
-  let urlPath = ''
-
   if (status === 'completed') {
-    urlPath = `/audit/report/${id}?type=${reportType}`
-  } else {
-    urlPath = `/audit/${id}`
-
-    if (axeId) {
-      urlPath += `?resultId=${axeId}`
-    }
+    return `/audit/report/${id}?type=${reportType}`
   }
-
-  return urlPath
+  return `/audit/${id}?resultId=${axeId}`
 }
