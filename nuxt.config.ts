e10947d70d6a877bdb155ecdc8afcb4efbe55be9
Robert Woźniak <cookiecodestudio@gmail.com>
2024-04-16 17:06:49 +0200

    Remove AXE_RUNNER_API_URL env usage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,6 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      axeRunnerApiUrl: process.env.AXE_RUNNER_API_URL,
       basicAuthUser: process.env.BASIC_AUTH_USER, // while only for dev keep it public
       basicAuthPassed: process.env.BASIC_AUTH_PASSWD, // while only for dev keep it public
     },
